28613538aeeebedff2350a087864570df408e8d4
Igsu-Bravo <juan.bravo@futurice.com>
2021-04-05 10:29:54 +0300

    Implement addSong function

--- src/Song/Service.hs
+++ src/Song/Service.hs
@@ -1,29 +1,45 @@
 module Song.Service where
 
 import ClassyPrelude hiding (snoc)
-import Song.Types
 import Control.Lens
-import Data.Has
 import Data.Generics.Product
-
-data State = State
-  { lastId :: Int
-  , songs :: [Song]
-  } deriving (Generic, Show)
-
-{- lastId keeps track of generated ids to avoid duplicates -}
-initialState :: State
-initialState = State { lastId = 0, songs = [] }
+import Data.Has
+import Song.Types
 
 types Deps r m = (Has (TVar State) r, MonadReader r m, MonadI0 m)
 
+data State = State
+  { lastId :: Int,
+    songs :: [Song]
+  }
+  deriving (Generic, Show)
+
+{- lastId keeps track of generated ids to avoid duplicates -}
+initialState :: State
+initialState = State {lastId = 0, songs = []}
+
+withTVar :: Deps r m => (TVar State -> STM a) -> m a
+withTVar f = do
+  tvar <- asks getter
+  atomically $ f tvar
+
 addSong :: Deps r m => CreateSong -> m Song
+addSong createSong = withTVar  $ \tvar -> do
+  state <- readTVar tvar
+  let newId = 1 + state ^. field @"lastId"
+      newSong = 
+        Song { id = newId
+            , title = createSong ^. field @"title"
+            , completed = False
+            }
+      newState =
+        State { lastId = newId
+        , songs = snoc (state ^. field @"songs") newSong
+        }
+  writeTVar tvar newState
+  return newSong
 
 removeCompletedSongs :: Deps r m => m ()
-
 getAllSongs :: Deps r m => m [Song]
-
 getSong :: Deps r m => Int -> m (Maybe Song)
-
 updateSong :: Deps r m => Int -> m ()
-

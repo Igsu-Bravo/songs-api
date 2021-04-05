f0e66d0d7a2235af02d1f590d9f375fa351b314d
Igsu-Bravo <juan.bravo@futurice.com>
2021-04-05 09:50:47 +0300

    Add extensions and types

---
+++ src/Song/Types.hs
@@ -0,0 +1,33 @@
+module Songs.Types where
+
+import ClassyPrelude
+import Data.Aeson
+import Data.Generics.Product
+import GHC.Generics (Generic)
+
+newtype CreateSong = CreateSong
+  { title :: Text
+  }
+  deriving (Generic, Show)
+
+instance ToJSON CreateSong
+
+instance FromJSON CreateSong
+
+data UpdateSong = UpdateSong
+  { title :: Text,
+    completed :: Bool
+  }
+  deriving (Generic, Show)
+
+instance ToJSON UpdateSong
+
+instance FromJSON UpdateSong
+
+data Song = Song
+  { id :: Int
+  , title :: Text
+  , completed :: Bool
+  } deriving (Generic, Show)
+
+instance ToJSON
